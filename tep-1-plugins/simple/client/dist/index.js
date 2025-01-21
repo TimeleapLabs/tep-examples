b035af7ef562d1a14440e4da3681fbffb177b665
Pouya <pouya@kenshi.io>
2025-01-22 00:22:34 +0100

    Switch to the official client

--- tep-1-plugins/simple/client/dist/index.js
+++ tep-1-plugins/simple/client/dist/index.js
@@ -1,70 +1,63 @@
-import WebSocket from "ws";
 import { Sia } from "@timeleap/sia";
 import { uuidv7obj } from "uuidv7";
 import * as ed from "@noble/ed25519";
 import { config } from "dotenv";
 import { base58_to_binary } from "base58-js";
+import { Client, Wallet } from "@timeleap/unchained-client";
 config();
 const VERSION = process.env.UNCHAINED_PROTOCOL_VERSION;
 const BROKER_PK = Buffer.from(base58_to_binary(process.env.BROKER_PUBLIC_KEY));
-const sk = base58_to_binary(process.env.CLIENT_PRIVATE_KEY);
-const pk = await ed.getPublicKeyAsync(sk);
-const ws = new WebSocket(`ws://localhost:9123/${VERSION}`);
+const sk = process.env.CLIENT_PRIVATE_KEY;
+const wallet = await Wallet.fromBase58(sk);
+const client = await Client.connect(wallet, `ws://localhost:9123/${VERSION}`);
 const age = Math.floor(Math.random() * 42) + 18;
-const opcode = new Uint8Array([6]);
 const uuid = uuidv7obj();
 const payload = new Sia()
-    .addByteArrayN(opcode)
+    .addByteArrayN(new Uint8Array([6]))
     .addByteArray8(uuid.bytes)
     .addAscii("swiss.timeleap.isWizard.v1")
     .addAscii("isWizard")
     .addUInt64(5000)
     .addAscii("John Doe")
     .addUInt8(age);
-const signature = await ed.signAsync(payload.toUint8ArrayReference(), sk);
-payload.addByteArrayN(pk).addByteArrayN(signature);
 console.log(`Checking if John Doe (${age}yo) is a wizard...`);
 console.log(`Request UUID: ${uuid.toString()}`);
-ws.on("open", () => {
-    ws.send(payload.toUint8Array());
-});
-ws.on("message", async (buf) => {
-    // read signature information
-    const auth = new Sia(buf.subarray(-96));
-    const signer = auth.readByteArrayN(32);
-    const signature = auth.readByteArrayN(64);
-    // signer should be the worker public key
-    if (!BROKER_PK.equals(signer)) {
-        console.log("Invalid signer");
-        return;
-    }
-    // verify the signature
-    const isValid = await ed.verifyAsync(signature, buf.subarray(0, buf.length - 96), signer);
-    if (!isValid) {
-        console.log("Invalid signature");
-        return;
-    }
-    // read the response
-    const sia = new Sia(new Uint8Array(buf));
-    const opcode = sia.readByteArrayN(1);
-    if (opcode[0] === 1) {
-        console.error(buf.subarray(1).toString());
-        return ws.close();
-    }
-    const responseUuid = sia.readByteArray8();
-    // verify that the response UUID matches the request UUID
-    if (Buffer.compare(uuid.bytes, responseUuid) !== 0) {
-        console.error("UUID mismatch");
-        return ws.close();
-    }
-    const error = sia.readUInt16();
-    if (error !== 0) {
-        console.error(`Error: ${error}`);
-        return ws.close();
-    }
-    const isWizard = sia.readBool();
-    const message = sia.readAscii();
-    console.log(message);
-    console.log(`Is Wizard: ${isWizard}`);
-    ws.close();
-});
+const response = await client.send(payload);
+// read signature information
+const auth = new Sia(response.subarray(-96));
+const signer = auth.readByteArrayN(32);
+const signature = auth.readByteArrayN(64);
+// signer should be the worker public key
+if (!BROKER_PK.equals(signer)) {
+    console.log("Invalid signer");
+    process.exit(1);
+}
+// verify the signature
+const isValid = await ed.verifyAsync(signature, response.subarray(0, response.length - 96), signer);
+if (!isValid) {
+    console.log("Invalid signature");
+    process.exit(1);
+}
+// read the response
+const sia = new Sia(response);
+const opcode = sia.readByteArrayN(1);
+if (opcode[0] === 1) {
+    console.error(response.subarray(1).toString());
+    process.exit(1);
+}
+const responseUuid = sia.readByteArray8();
+// verify that the response UUID matches the request UUID
+if (Buffer.compare(uuid.bytes, responseUuid) !== 0) {
+    console.error("UUID mismatch");
+    process.exit(1);
+}
+const error = sia.readUInt16();
+if (error !== 0) {
+    console.error(`Error: ${error}`);
+    process.exit(1);
+}
+const isWizard = sia.readBool();
+const message = sia.readAscii();
+console.log(message);
+console.log(`Is Wizard: ${isWizard}`);
+client.close();
